raspbian-packages/xen

commit 49ab17a3a615e1ab4ccc46d6942f925cf841df4b

tools: xl: handle unspecified extra= when dealing with root=
If the cfg file includes root= but not extra= (nor cmdline=, which
supercedes both) then the command line will end up with an extra
"(null)" on it (at least with glibc's implementation of asprintf).

Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
Cc: Chunyan Liu <cyliu@suse.com>
Acked-by: Wei Liu <wei.liu2@citrix.com>

tools/libxl/xl_cmdimpl.c
@@ -813,9 +813,11 @@ static char *parse_cmdline(XLU_Config *config)
             fprintf(stderr, "Warning: ignoring root= and extra= "
                     "in favour of cmdline=\n");
     } else {
-        if (root) {
+        if (root && extra) {
             if (asprintf(&cmdline, "root=%s %s", root, extra) == -1)
                 cmdline = NULL;
+        } else if (root) {
+            cmdline = strdup(root);
         } else if (extra) {
             cmdline = strdup(extra);
         }

commit 1f69faa922320c2ebc75deedf93ed3eddc866515

tools: xl: prepend root= to root field on command line with no extra
This was broken in 49ab17a3a615 "tools: xl: handle unspecified extra=
when dealing with root=".

Reported-by: Sander Eikelenboom <linux@eikelenboom.it>
Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
Tested-by: Sander Eikelenboom <linux@eikelenboom.it>
Acked-by: Wei Liu <wei.liu2@citrix.com>

tools/libxl/xl_cmdimpl.c
@@ -817,7 +817,8 @@ static char *parse_cmdline(XLU_Config *config)
             if (asprintf(&cmdline, "root=%s %s", root, extra) == -1)
                 cmdline = NULL;
         } else if (root) {
-            cmdline = strdup(root);
+            if (asprintf(&cmdline, "root=%s", root) == -1)
+                cmdline = NULL;
         } else if (extra) {
             cmdline = strdup(extra);
         }