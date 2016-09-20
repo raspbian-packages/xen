raspbian-packages/xen

commit 3a7f872ae427c2846b2923d76236464b6e8e8145

tools: lift BUILD_BUG_ON to a tools header file
Only define BUILD_BUG_ON when there isn't one already, because mini-os
currently leaks that.

Signed-off-by: Wei Liu <wei.liu2@citrix.com>
Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

tools/firmware/hvmloader/rombios.c
@@ -31,6 +31,7 @@
 #include "option_rom.h"
 
 #include <xen/hvm/params.h>
+#include <xen-tools/libs.h>
 
 #define ROM_INCLUDE_ROMBIOS
 #define ROM_INCLUDE_VGABIOS

tools/firmware/hvmloader/smbios.c
@@ -26,6 +26,7 @@
 #include "util.h"
 #include "hypercall.h"
 #include <xen/hvm/hvm_xs_strings.h>
+#include <xen-tools/libs.h>
 
 /* SBMIOS handle base values */
 #define SMBIOS_HANDLE_TYPE0   0x0000

tools/firmware/hvmloader/util.h
@@ -41,7 +41,6 @@ void __assert_failed(char *assertion, char *file, int line)
 void __bug(char *file, int line) __attribute__((noreturn));
 #define BUG() __bug(__FILE__, __LINE__)
 #define BUG_ON(p) do { if (p) BUG(); } while (0)
-#define BUILD_BUG_ON(p) ((void)sizeof(char[1 - 2 * !!(p)]))
 
 #define min_t(type,x,y) \
         ({ type __x = (x); type __y = (y); __x < __y ? __x: __y; })

tools/include/xen-tools/libs.h
@@ -1,0 +1,11 @@
+#ifndef __XEN_TOOLS_LIBS__
+
+#ifndef BUILD_BUG_ON
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#define BUILD_BUG_ON(p) ({ _Static_assert(!(p), "!(" #p ")"); })
+#else
+#define BUILD_BUG_ON(p) ((void)sizeof(char[1 - 2 * !!(p)]))
+#endif
+#endif
+
+#endif	/* __XEN_TOOLS_LIBS__ */

tools/libxc/xc_core_arm.c
@@ -19,6 +19,8 @@
 #include "xg_private.h"
 #include "xc_core.h"
 
+#include <xen-tools/libs.h>
+
 int
 xc_core_arch_gpfn_may_present(struct xc_core_arch_context *arch_ctxt,
                               unsigned long pfn)
@@ -101,7 +103,7 @@ xc_core_arch_get_scratch_gpfn(xc_interface *xch, domid_t domid,
      * The Grant Table region space is not used until the guest is
      * booting. Use the first page for the scratch pfn.
      */
-    XC_BUILD_BUG_ON(GUEST_GNTTAB_SIZE < XC_PAGE_SIZE);
+    BUILD_BUG_ON(GUEST_GNTTAB_SIZE < XC_PAGE_SIZE);
 
     *gpfn = GUEST_GNTTAB_BASE >> XC_PAGE_SHIFT;
 

tools/libxc/xc_cpuid_x86.c
@@ -25,6 +25,7 @@
 #include "xc_private.h"
 #include "xc_bitops.h"
 #include <xen/hvm/params.h>
+#include <xen-tools/libs.h>
 
 enum {
 #define XEN_CPUFEATURE(name, value) X86_FEATURE_##name = value,
@@ -98,12 +99,12 @@ const uint32_t *xc_get_static_cpu_featuremask(
         hvm_hap[FEATURESET_NR_ENTRIES] = INIT_HVM_HAP_FEATURES,
         deep_features[FEATURESET_NR_ENTRIES] = INIT_DEEP_FEATURES;
 
-    XC_BUILD_BUG_ON(ARRAY_SIZE(known) != FEATURESET_NR_ENTRIES);
-    XC_BUILD_BUG_ON(ARRAY_SIZE(special) != FEATURESET_NR_ENTRIES);
-    XC_BUILD_BUG_ON(ARRAY_SIZE(pv) != FEATURESET_NR_ENTRIES);
-    XC_BUILD_BUG_ON(ARRAY_SIZE(hvm_shadow) != FEATURESET_NR_ENTRIES);
-    XC_BUILD_BUG_ON(ARRAY_SIZE(hvm_hap) != FEATURESET_NR_ENTRIES);
-    XC_BUILD_BUG_ON(ARRAY_SIZE(deep_features) != FEATURESET_NR_ENTRIES);
+    BUILD_BUG_ON(ARRAY_SIZE(known) != FEATURESET_NR_ENTRIES);
+    BUILD_BUG_ON(ARRAY_SIZE(special) != FEATURESET_NR_ENTRIES);
+    BUILD_BUG_ON(ARRAY_SIZE(pv) != FEATURESET_NR_ENTRIES);
+    BUILD_BUG_ON(ARRAY_SIZE(hvm_shadow) != FEATURESET_NR_ENTRIES);
+    BUILD_BUG_ON(ARRAY_SIZE(hvm_hap) != FEATURESET_NR_ENTRIES);
+    BUILD_BUG_ON(ARRAY_SIZE(deep_features) != FEATURESET_NR_ENTRIES);
 
     switch ( mask )
     {
@@ -139,7 +140,7 @@ const uint32_t *xc_get_feature_deep_deps(uint32_t feature)
 
     unsigned int start = 0, end = ARRAY_SIZE(deep_deps);
 
-    XC_BUILD_BUG_ON(ARRAY_SIZE(deep_deps) != NR_DEEP_DEPS);
+    BUILD_BUG_ON(ARRAY_SIZE(deep_deps) != NR_DEEP_DEPS);
 
     /* deep_deps[] is sorted.  Perform a binary search. */
     while ( start < end )

tools/libxc/xc_dom_arm.c
@@ -21,6 +21,7 @@
 
 #include <xen/xen.h>
 #include <xen/io/protocols.h>
+#include <xen-tools/libs.h>
 
 #include "xg_private.h"
 #include "xc_dom.h"
@@ -69,7 +70,7 @@ static int alloc_magic_pages(struct xc_dom_image *dom)
     const xen_pfn_t base = GUEST_MAGIC_BASE >> XC_PAGE_SHIFT;
     xen_pfn_t p2m[NR_MAGIC_PAGES];
 
-    XC_BUILD_BUG_ON(NR_MAGIC_PAGES > GUEST_MAGIC_SIZE >> XC_PAGE_SHIFT);
+    BUILD_BUG_ON(NR_MAGIC_PAGES > GUEST_MAGIC_SIZE >> XC_PAGE_SHIFT);
 
     DOMPRINTF_CALLED(dom->xch);
 

tools/libxc/xc_dom_bzimageloader.c
@@ -34,6 +34,8 @@
 #include "xg_private.h"
 #include "xc_dom_decompress.h"
 
+#include <xen-tools/libs.h>
+
 #ifndef __MINIOS__
 
 #if defined(HAVE_BZLIB)
@@ -418,7 +420,7 @@ static int xc_try_lzo1x_decode(
      * lzo_uint should match size_t. Check that this is the case to be
      * sure we won't overflow various lzo_uint fields.
      */
-    XC_BUILD_BUG_ON(sizeof(lzo_uint) != sizeof(size_t));
+    BUILD_BUG_ON(sizeof(lzo_uint) != sizeof(size_t));
 
     ret = lzo_init();
     if ( ret != LZO_E_OK )

commit 8897e05f41adc35ecf3bca915b48c0768a041587

Fix issues introduced in 3a7f872a
3a7f872a ("tools: lift BUILD_BUG_ON to a tools header file") was taken
out from an rather old half finished branch by dropping unrelated
changes.  Unfortunately two issues sneaked in.

1. Hvmloader should be standalone. Revert the changes to hvmloader.
2. The define guard in libs.h was erroneously deleted. Add that back.

Signed-off-by: Wei Liu <wei.liu2@citrix.com>
Acked-by: Jan Beulich <jbeulich@suse.com>
Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

tools/firmware/hvmloader/rombios.c
@@ -31,7 +31,6 @@
 #include "option_rom.h"
 
 #include <xen/hvm/params.h>
-#include <xen-tools/libs.h>
 
 #define ROM_INCLUDE_ROMBIOS
 #define ROM_INCLUDE_VGABIOS

tools/firmware/hvmloader/smbios.c
@@ -26,7 +26,6 @@
 #include "util.h"
 #include "hypercall.h"
 #include <xen/hvm/hvm_xs_strings.h>
-#include <xen-tools/libs.h>
 
 /* SBMIOS handle base values */
 #define SMBIOS_HANDLE_TYPE0   0x0000

tools/firmware/hvmloader/util.h
@@ -41,6 +41,7 @@ void __assert_failed(char *assertion, char *file, int line)
 void __bug(char *file, int line) __attribute__((noreturn));
 #define BUG() __bug(__FILE__, __LINE__)
 #define BUG_ON(p) do { if (p) BUG(); } while (0)
+#define BUILD_BUG_ON(p) ((void)sizeof(char[1 - 2 * !!(p)]))
 
 #define min_t(type,x,y) \
         ({ type __x = (x); type __y = (y); __x < __y ? __x: __y; })

tools/include/xen-tools/libs.h
@@ -1,4 +1,5 @@
 #ifndef __XEN_TOOLS_LIBS__
+#define __XEN_TOOLS_LIBS__
 
 #ifndef BUILD_BUG_ON
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)