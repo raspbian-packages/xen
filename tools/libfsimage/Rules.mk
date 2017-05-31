448a00f2f51befc8fadbaba8fa1776cd7ca53d68
Debian Xen Team <pkg-xen-devel@lists.alioth.debian.org>
2017-05-31 07:52:47 +0000

    tools-libfsimage-prefix
    Gbp-Pq: Name tools-libfsimage-prefix.diff

--- tools/libfsimage/Rules.mk
+++ tools/libfsimage/Rules.mk
@@ -6,12 +6,7 @@ LDFLAGS += -L../common/
 
 PIC_OBJS := $(patsubst %.c,%.opic,$(LIB_SRCS-y))
 
-FSDIR-$(CONFIG_Linux) = $(LIBDIR)/fs/$(FS)
-FSDIR-$(CONFIG_SunOS)-x86_64 = $(PREFIX)/lib/fs/$(FS)/64
-FSDIR-$(CONFIG_SunOS)-x86_32 = $(PREFIX)/lib/fs/$(FS)/
-FSDIR-$(CONFIG_SunOS) = $(FSDIR-$(CONFIG_SunOS)-$(XEN_TARGET_ARCH))
-FSDIR-$(CONFIG_NetBSD) = $(LIBDIR)/fs/$(FS)
-FSDIR = $(FSDIR-y)
+FSDIR = $(PRIVATE_LIBDIR)/fs/$(FS)
 
 FSLIB = fsimage.so
 
