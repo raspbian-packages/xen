f864a00a44e862fd2b6c638f5ec8d75e10cd76d2
Jan Beulich <jbeulich@suse.com>
2017-06-02 12:22:42 +0100

    gnttab: fix unmap pin accounting race
    Once all {writable} mappings of a grant entry have been unmapped, the
    hypervisor informs the guest that the grant entry has been released by
    clearing the _GTF_{reading,writing} usage flags in the guest's grant
    table as appropriate.
    
    Unfortunately, at the moment, the code that updates the accounting
    happens in a different critical section than the one which updates the
    usage flags; this means that under the right circumstances, there may be
    a window in time after the hypervisor reported the grant as being free
    during which the grant referee still had access to the page.
    
    Move the grant accounting code into the same critical section as the
    reporting code to make sure this kind of race can't happen.
    
    This is part of XSA-218.
    
    Reported-by: Jann Horn <jannh.com>
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    
    Gbp-Pq: Name 0004-gnttab-fix-unmap-pin-accounting-race.patch

--- xen/drivers/passthrough/iommu.c
+++ xen/drivers/passthrough/iommu.c
@@ -253,21 +253,47 @@ int iommu_map_page(struct domain *d, unsigned long gfn, unsigned long mfn,
                    unsigned int flags)
 {
     struct hvm_iommu *hd = domain_hvm_iommu(d);
+    int rc;
 
     if ( !iommu_enabled || !hd->platform_ops )
         return 0;
 
-    return hd->platform_ops->map_page(d, gfn, mfn, flags);
+    rc = hd->platform_ops->map_page(d, gfn, mfn, flags);
+    if ( unlikely(rc) )
+    {
+        if ( !d->is_shutting_down && printk_ratelimit() )
+            printk(XENLOG_ERR
+                   "d%d: IOMMU mapping gfn %#lx to mfn %#lx failed: %d\n",
+                   d->domain_id, gfn, mfn, rc);
+
+        if ( !is_hardware_domain(d) )
+            domain_crash(d);
+    }
+
+    return rc;
 }
 
 int iommu_unmap_page(struct domain *d, unsigned long gfn)
 {
     struct hvm_iommu *hd = domain_hvm_iommu(d);
-
+    int rc;
+    
     if ( !iommu_enabled || !hd->platform_ops )
         return 0;
 
-    return hd->platform_ops->unmap_page(d, gfn);
+    rc = hd->platform_ops->unmap_page(d, gfn);
+    if ( unlikely(rc) )
+    {
+        if ( !d->is_shutting_down && printk_ratelimit() )
+            printk(XENLOG_ERR
+                   "d%d: IOMMU unmapping gfn %#lx failed: %d\n",
+                   d->domain_id, gfn, rc);
+
+        if ( !is_hardware_domain(d) )
+            domain_crash(d);
+    }
+
+    return rc;
 }
 
 /* caller should hold the pcidevs_lock */
