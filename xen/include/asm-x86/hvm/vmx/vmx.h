3455e0c004d4f014133a5739725919c38e7fa88c
Debian Xen Team <pkg-xen-devel@lists.alioth.debian.org>
2018-05-25 09:48:35 +0100

    xen-x86-interrupt-pointer-missmatch
    Gbp-Pq: Name xen-x86-interrupt-pointer-missmatch.diff

--- xen/include/asm-x86/hvm/vmx/vmx.h
+++ xen/include/asm-x86/hvm/vmx/vmx.h
@@ -63,7 +63,7 @@ typedef union {
 
 void vmx_asm_vmexit_handler(struct cpu_user_regs);
 void vmx_asm_do_vmentry(void);
-void vmx_intr_assist(void);
+asmlinkage void vmx_intr_assist(void);
 void vmx_do_resume(struct vcpu *);
 void vmx_vlapic_msr_changed(struct vcpu *v);
 void vmx_realmode(struct cpu_user_regs *regs);
